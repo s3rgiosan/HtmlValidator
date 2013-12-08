0f0dc95b1125139cfd06eddb20883ddcdd60e40c
s3rgiosan <s3rgiosan@gmail.com>
2013-12-08 00:00:10 +0000

    Update author info

--- HtmlValidator.py
+++ HtmlValidator.py
@@ -5,8 +5,8 @@
 
   @version 1.0.0
   @author  Sérgio Santos
-  @email   hello@sergiosantos.me
-  @url     http://www.sergiosantos.me
+  @email   me@s3rgiosan.com
+  @url     http://s3rgiosan.com
 '''
 import sublime, sublime_plugin, os
 
